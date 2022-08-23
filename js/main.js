315ededfc8057bea1a07a71803809d0fd7087094
Lucas Cascardo <lucascascardo.2@gmail.com>
2022-08-23 19:53:18 -0300

    Inicio desafio 2 de JS

--- js/main.js
+++ js/main.js
@@ -1,16 +1,51 @@
-// ALGORITMO DONDE LE PIDE AL USUARIO QUE INGRESE INGREDIENTES PARA SU SANDWICH, 
-// PARA LUEGO MOSTRARLE UNA ALERTA MENCIONANDO DICHOS INGREDIENTES. 
-// AL INGRESAR ESC (EN MAYUSCULAS) SALE DEL CICLO. 
-// SI EL USUSARIO PIDE EL INGREDIENTE MAYONESA (EN MAYUSCULAS) LE DA UNA ALERTA DICIENDO QUE NO HAY.
-let sandwich = "";
-let ingredientes = prompt("INGRESAR INGREDIENTES PARA TU SANDWICH, PARA SALIR INGRESE ESC");
-while(ingredientes!="ESC"){
-    if(ingredientes == "MAYONESA"){
-        alert("NO TENEMOS MAYONESA");
+// DECLARO VARIABLES
+let producto = 0;
+let carrito = 0;
+
+// FUNCIONES:
+// FUNCION PARA SELECCIONAR PRODUCTOS
+function seleccionar() {
+    producto = parseInt(prompt("Ingrese un numero dependiendo del producto a seleccionar:\n 1-Procesador Intel $5.000\n 2-Procesador AMD $3.500\n 3-Placa de video Nvidia $40.000\n 4-Placa de video AMD $30.000\n 5-Gabinete $10.000\n INGRESAR NUMERO 0 PARA TERMINAR DE SELECCIONAR"));
+    return producto;
+};
+// FUNCION PARA SUMAR PRODUCTOS
+function sumar() {
+    switch (producto) {
+        case 1:
+            carrito += 5000;
+            break;
+        case 2:
+            carrito += 3500;
+            break;
+        case 3:
+            carrito += 40000;
+            break;
+        case 4:
+            carrito += 30000;
+            break;
+        case 5:
+            carrito += 10000;
+            break;
+        default:
+            alert("Producto inexistente, vuelva a ingresar un producto");
+            break;
     }
-    else{
-        sandwich += ingredientes + " ";
-    }
-    ingredientes = prompt("INGRESAR INGREDIENTES PARA TU SANDWICH, PARA SALIR INGRESE ESC");
+    return carrito;
 }
-alert("SU SANDWICH TIENE: " + sandwich);
+// FUNCION PARA REALIZAR DESCUENTO DE 20% SI LA COMPRA ES MAYOR O IGUAL A $50.000
+function descuento20() {
+    if (carrito >= 50000) {
+        carrito = carrito - (carrito * 0.2);
+        return carrito;
+    }
+};
+
+// PROCESO
+seleccionar();
+while (producto != 0) {
+    sumar();
+    seleccionar();
+}
+descuento20();
+alert("El costo total a pagar es de: " + "$" + carrito);
+
